1e6e401ba0be35dbd627d33c138276c06ac0b61e
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-01-27 18:12:56 +0000

    Adding the Spammer Model

---
+++ src/Bases/Model.php
@@ -0,0 +1,14 @@
+<?php namespace Arcanesoft\Seo\Bases;
+
+use Arcanedev\Support\Bases\Model as BaseModel;
+
+/**
+ * Class     Model
+ *
+ * @package  Arcanesoft\Seo\Bases
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+abstract class Model extends BaseModel
+{
+    //
+}
---
+++ src/Models/Spammer.php
@@ -0,0 +1,14 @@
+<?php namespace Arcanesoft\Seo\Models;
+
+use Arcanedev\Support\Bases\Model;
+
+/**
+ * Class     Spammer
+ *
+ * @package  Arcanesoft\Seo\Models
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class Spammer extends Model
+{
+    //
+}
